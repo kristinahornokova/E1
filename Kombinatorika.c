b6c191790c8df2618bd7872c378c020b4fedc26f
kristinahornokova <161163813+kristinahornokova@users.noreply.github.com>
2024-04-01 14:21:38 +0200

    doplnenie main

--- Kombinatorika.c
+++ Kombinatorika.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-main (){
-    
+main(){
+    int cislo;
+    printf("Zadaj cislo: ");
+    if (scanf ("%d", &cislo) !=1){
+        printf("Chyba: Zadajte platne cele cislo. \n");
+        return 1;
+    }
+    return 0;
 }
