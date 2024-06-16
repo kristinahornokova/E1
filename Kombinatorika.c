91b261fd67e065077fcc7a356fdbf391f450f6bc
kristinahornokova <161163813+kristinahornokova@users.noreply.github.com>
2024-06-16 19:40:30 +0200

    optimalizácia kódu

--- Kombinatorika.c
+++ Kombinatorika.c
@@ -5,27 +5,27 @@
 #define FAIL 0
 
 void vypis_kompoziciu(int *kompozicia, int pocet) {
-    for (int i = 0; i < pocet; i++) {
-        if (kompozicia[i] != 0) {printf("%d", kompozicia[i]);
-        
+    for (int i = 0; i < pocet; i++) {if (kompozicia[i] != 0) {
+        printf("%d", kompozicia[i]);
         if (i < pocet - 1)
             printf(" + ");
         }
     }
     printf("\n");
-   
 }
 
 void generuj_kompozicie(int n) {
-    if (n > 0 && n < 11) {
+    if (n > 2 && n < 11) {
         printf("Zacinam pocitat kompozicie.\n");
+        printf("%d\n",n);
+        printf("%d + 1\n",n - 1);
+        printf("1 + %d\n",n - 1);
     }
     else {
-        printf("Chyba: Cislo musi byt vacsie ako 0.\n");
+        printf("Chyba: Cislo musi byt vacsie ako 2.\n");
         return;
         }
-    
-    int *kompozicia = (int *)malloc(n * sizeof(int)); 
+    int *kompozicia = (int *)malloc(n * sizeof(int));
     if (kompozicia == NULL) {
         printf("Chyba: Nedostatok pamate.\n");
         return;
@@ -36,41 +36,34 @@ void generuj_kompozicie(int n) {
         kompozicia[i] = 0;
     }
     int sucet = 0;
-    int index = n - 1;
-
     while (1) {
-        if (sucet == n) {
-            vypis_kompoziciu(kompozicia, n);
-        }
-        
+        int index = n - 1;
         int kontrola = 1;
         sucet = 0;
         while (index >= 0 && kontrola) {
             kompozicia[index]++;
             kontrola = 0;
-            if (kompozicia[index] > n) {
+            if (kompozicia[index] > n - 2) {
                 kompozicia[index] = 1;
                 kontrola = 1;
                 index = index - 1;
             }
         }
-        if (index < 0) break;
-        
         for (int i = 0; i < n; i++) {
             sucet = sucet + kompozicia[i];
         }
-        index = n - 1;
+        if (sucet == n) {
+            vypis_kompoziciu(kompozicia, n);
+        }
+        if (index == 0) break;
     }
-
     free(kompozicia); // Uvolnenie pamate
     return;
 }
 
-
-
 int main(void){
     int cislo;
-    printf("Zadaj cislo od 1 do 10: ");
+    printf("Zadaj cislo od 3 do 10: ");
     if (scanf ("%d", &cislo) !=1){
         printf("Chyba: Zadajte platne cele cislo. \n");
         return FAIL;
